6b5eacebac7f43abb34c9ecfd3f0a2f5ac3c93c9
Naoki Kishi <naokikishi.kyoto@gmail.com>
2019-02-12 15:23:40 +0900

    コマンドラインでチャットができるようになった

---
+++ README.md
@@ -0,0 +1,6 @@
+# Realtime Chat Server using GraphQL Subscription and Redis
+
+## References
+- [Real-time Chat with GraphQL Subscriptions in Go](https://outcrawl.com/go-graphql-realtime-chat)
+- [GoとRedisにおける簡単なチャットアプリケーション](https://medium.com/eureka-engineering/go-redis-application-28c8c793a652)
+- [Redis の Pub/Sub を使って Node.js + WebSocket のスケールアウトを実現する方法](https://blog.dakatsuka.jp/2011/06/19/nodejs-redis-pubsub.html)
--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/go-redis/redis"
+	"os"
+	"time"
 )
 
+// NewRedisClient returns a client for redis.
 func NewRedisClient() *redis.Client {
 	client := redis.NewClient(&redis.Options{
-		Addr:     "redis:6379",
+		Addr:     "localhost:6379",
 		Password: "",
 		DB:       0,
 	})
@@ -22,11 +26,91 @@ func NewRedisClient() *redis.Client {
 
 func main() {
 
+	if len(os.Args) != 2 {
+		fmt.Println("There is an error in the argument")
+		os.Exit(1)
+	}
+	userName := os.Args[1]
+	userKey := "online" + userName
+
 	client := NewRedisClient()
 	defer client.Close()
-	pong, err := client.Ping().Result()
+
+	val, err := client.SetNX(userKey, userName, 2*time.Minute).Result()
+	defer client.Del(userKey)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	fmt.Println(pong)
+
+	if val == false {
+		fmt.Println("User already online")
+		os.Exit(1)
+	}
+
+	// 受け取ったメッセージをsubChanに流すgoroutine
+	subChan := make(chan string)
+
+	go func() {
+		clientForRoom := NewRedisClient()
+		defer clientForRoom.Close()
+
+		pubsub := clientForRoom.Subscribe("room")
+		defer pubsub.Close()
+
+		for {
+			msgi, err := pubsub.Receive()
+			if err != nil {
+				panic(err)
+			}
+
+			switch msg := msgi.(type) {
+			case *redis.Subscription:
+				break
+			case *redis.Message:
+				subChan <- fmt.Sprint(msg.Payload)
+
+			default:
+				panic("unreached")
+			}
+		}
+	}()
+
+	// コマンドプロンプトで受け取った入力をsayChanに流すgoroutine
+	sayChan := make(chan string)
+	go func() {
+		bio := bufio.NewReader(os.Stdin)
+
+		for {
+			line, _, err := bio.ReadLine()
+			if err != nil {
+				fmt.Println(err)
+				sayChan <- "/exit"
+				return
+			}
+			sayChan <- string(line)
+		}
+	}()
+
+	client.Publish("room", userName+" has joined")
+	defer client.Publish("room", userName+" has left")
+
+	chatExit := false
+
+	for !chatExit {
+		prompt := ">"
+		fmt.Print(prompt)
+
+		select {
+		case msg := <-subChan:
+			fmt.Println(msg)
+
+		case line := <-sayChan:
+			if line == "/exit" {
+				chatExit = true
+				return
+			}
+			client.Publish("room", userName+": "+line)
+		}
+	}
 }
